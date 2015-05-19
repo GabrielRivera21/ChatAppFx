acb0a16aca56ac95df49a3588b10cc75573be47a
Gabriel Rivera Per-ossenkopp <gabriel.rivera2192@gmail.com>
2015-05-19 04:33:04 -0400

    Added build.fxbuild for java FX

--- src/edu/uprb/chat/ChatClient.java
+++ src/edu/uprb/chat/ChatClient.java
@@ -9,33 +9,33 @@ import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class ChatClient extends Application {
-	
+
 	private Stage primaryStage;
 	private VBox chatLayout;
-	
+
 	@Override
 	public void start(Stage primaryStage) {
 		this.primaryStage = primaryStage;
 		this.primaryStage.setTitle("Data Communication Chat App");
-		
+
 		initChatLayout();
 	}
 
 	private void initChatLayout() {
 		try {
-            // Load root layout from fxml file.
-            FXMLLoader loader = new FXMLLoader();
-            loader.setLocation(ChatClient.class.getResource("view/ClientGUI.fxml"));
-            loader.setController(new ChatController());
-            chatLayout = (VBox) loader.load();
+			// Load root layout from fxml file.
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(ChatClient.class.getResource("view/ClientGUI.fxml"));
+			loader.setController(new ChatController());
+			chatLayout = (VBox) loader.load();
 
-            // Show the scene containing the root layout.
-            Scene scene = new Scene(chatLayout);
-            primaryStage.setScene(scene);
-            primaryStage.show();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+			// Show the scene containing the root layout.
+			Scene scene = new Scene(chatLayout);
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 	public static void main(String[] args) {
