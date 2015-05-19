7ac90ab8306c386fafab8c96111751cc217210ae
Gabriel Rivera Per-ossenkopp <gabriel.rivera2192@gmail.com>
2015-05-19 04:28:35 -0400

    Inital Commit

---
+++ src/edu/uprb/chat/ChatClient.java
@@ -0,0 +1,44 @@
+package edu.uprb.chat;
+
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class ChatClient extends Application {
+	
+	private Stage primaryStage;
+	private VBox chatLayout;
+	
+	@Override
+	public void start(Stage primaryStage) {
+		this.primaryStage = primaryStage;
+		this.primaryStage.setTitle("Data Communication Chat App");
+		
+		initChatLayout();
+	}
+
+	private void initChatLayout() {
+		try {
+            // Load root layout from fxml file.
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(ChatClient.class.getResource("view/ClientGUI.fxml"));
+            loader.setController(new ChatController());
+            chatLayout = (VBox) loader.load();
+
+            // Show the scene containing the root layout.
+            Scene scene = new Scene(chatLayout);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
---
+++ src/edu/uprb/chat/ChatController.java
@@ -0,0 +1,28 @@
+package edu.uprb.chat;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class ChatController {
+	
+	@FXML
+	private Button btnLogin;
+	
+	@FXML
+	private Button btnLogout;
+	
+	public void sendMessage() {
+		//TODO: Send Msgs
+		System.out.println("Send Message.");
+	}
+	
+	public void login() {
+		//TODO
+		System.out.println("Login.");
+	}
+	
+	public void logout() {
+		//TODO
+		System.out.println("Logout");
+	}
+}
